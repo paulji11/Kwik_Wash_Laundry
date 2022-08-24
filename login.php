d19f87e740f48a7fb30de4dad6711c58c4defe87
paulji11 <95049533+paulji11@users.noreply.github.com>
2022-08-24 09:29:07 +0530

    final folder

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+   <div class="log_form">
+    <form method="post">
+    <div class="login_box">
+      <input type="text" name="log_email" placeholder="email"> <br>
+      <input type="password" name="log_pass" placeholder="password"> <br>
+      <input type="submit" value="Login">
+      </div>
+    </form>
+   </div>
+</body>
+</html>
